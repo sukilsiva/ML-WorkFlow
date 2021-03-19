47340916df9647157921aed08d43f03cca6ee4f3
Sukil Siva <sukilsiva71@outlook.com>
2021-03-20 01:39:04 +0530

    Setup File Done

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="ChurnPrediction",
+    version="0.0.1",
+    description="its a Churn C package", 
+    author="SukilSiva", 
+    packages=find_packages(),
+    license="MIT"
+)
